6011a768a90b048f051c734c45315916cec281a7
cecilia <stephy.barboza@gmail.com>
2019-11-30 13:09:37 -0300

    Cambio a GeoPandas

--- tesis/script.py
+++ tesis/script.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import copy
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
@@ -17,53 +18,51 @@ area = barrios_caba['area'].sum()
 
 class Individuo:
     # un arreglo de 1 eje, con forma (1, 50) una fila, 50 columnas
-    def __init__(self, q_coordenadas):
-        self.coordenadas = np.empty(shape=(1, q_coordenadas), dtype=Point)
+    def __init__(self):
+        self.coordenadas = None
         self.fitness = 0
 
     def validar_coordenada(self, punto):
         return barrios_caba.contains(punto).any()
 
-    def crear_coordenadas(self):
-        for i in range(0, self.coordenadas.size):
+    def crear_coordenadas(self, q_coordenadas):
+        puntos = []
+        for i in range(0, q_coordenadas):
             x_min, y_min, x_max, y_max = barrios_caba.total_bounds
             while True:
                 x = np.random.uniform(x_min, x_max)
                 y = np.random.uniform(y_min, y_max)
                 point = Point(x, y)
                 if self.validar_coordenada(point):
-                    self.coordenadas[0, i] = point
+                    puntos.append(point)
                     break
+        self.coordenadas = gpd.GeoSeries(puntos)
 
     def calcular_nni(self):
-        to_radians = lambda c: (np.radians(c.x), np.radians(c.y))
-        vectorized_to_radians = np.vectorize(to_radians)
-        puntos = vectorized_to_radians(self.coordenadas)
-        x = puntos[0].tolist()[0]
-        y = puntos[1].tolist()[0]
-        puntos_radianes = np.array(list(zip(x, y)))
-
-        nbrs = NearestNeighbors(n_neighbors=2, metric='haversine').fit(puntos_radianes)
-        distances, indices = nbrs.kneighbors(puntos_radianes)
+        coord_radianes = copy.deepcopy(self.coordenadas)
+        coord_radianes = coord_radianes.apply(lambda c: (np.radians(c.x), np.radians(c.y)))
+        nbrs = NearestNeighbors(n_neighbors=2, metric='haversine').fit(coord_radianes.tolist())
+        distances, indices = nbrs.kneighbors(coord_radianes.tolist())
         distances = distances * 6371000 / 1000
-        # este array contiene para c/ punto, la distancia con respecto a su vecino más próximo
         result = distances[:, 1]
-        dobs = np.sum(result) / len(puntos_radianes)
-        rn = dobs / (0.5 * np.sqrt((area / len(puntos_radianes))))
+        dobs = np.sum(result) / len(coord_radianes.tolist())
+        rn = dobs / (0.5 * np.sqrt((area / len(coord_radianes.tolist()))))
         self.fitness = rn
 
 
 class Poblacion:
-    def __init__(self, q_individuos, q_coordenadas):
-        self.individuos = np.empty(shape=(q_individuos, 1), dtype=Individuo)
-        self.q_coordenadas = q_coordenadas
+    def __init__(self):
+        self.individuos = None
 
-    def crear_individuos(self):
-        for i in range(0, self.individuos.size):
-            individuo = Individuo(self.q_coordenadas)
-            individuo.crear_coordenadas()
-            individuo.calcular_nni()
-            self.individuos[i, 0] = individuo
+    def crear_individuos(self, q_individuos, q_coordenadas):
+        individuos = []
+        for i in range(0, q_individuos):
+            ind = Individuo()
+            ind.crear_coordenadas(q_coordenadas)
+            ind.calcular_nni()
+            individuos.append(ind)
+
+        self.individuos = gpd.GeoSeries(individuos)
 
 
 class AlgoritmoGenetico:
@@ -76,12 +75,19 @@ class AlgoritmoGenetico:
         self.prob_mut_alelo = 0.2
         self.mu = 0
         self.sigma = 1
+        self.poblacion = None
 
     def inicio_ga(self):
-        poblacion = Poblacion(self.q_individuos, self.q_coordenadas)
-        poblacion.crear_individuos()
-        return poblacion
+        self.poblacion = Poblacion(self.q_individuos, self.q_coordenadas)
+        self.poblacion.crear_individuos()
+
+    def seleccion_ruleta(self):
+        for ind in self.poblacion.individuos:
+            print(type(ind))
 
 
-ga = AlgoritmoGenetico()
-ga.inicio_ga()
+pob = Poblacion()
+pob.crear_individuos(10, 50)
+for ind in pob.individuos:
+    print('Individuo 1')
+    print(ind)
